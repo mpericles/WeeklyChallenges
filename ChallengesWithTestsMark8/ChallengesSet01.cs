3312c96cbbcc55acf5864e9ad7eefb58d7e6cdfb
mpericles <mpericles1@yahoo.com>
2023-06-27 11:25:26 -0400

    first weekly challenge

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,6 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
+            //Complete
             if(num1 == num2)
             {
                 return true;
@@ -18,32 +19,64 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //Complete
+            double answer = minuend - subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //Complete
+            int answer;
+            answer = number1 + number2;
+            return answer;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //Complete
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else return number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //Complete
+            long answer;
+            answer = factor1 * factor2;
+            return answer;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //In Progress
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+            //string expectedGreeting = "Hello!";
+            //Console.WriteLine("Write your name");
+            //nameOfPerson = Console.ReadLine();
+            //for (nameOfPerson != null);
+            //{
+            //    Console.WriteLine($"Hello, {nameOfPerson}!");
+            //} 
+            //return expectedGreeting;
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            //Complete
+            //Console.WriteLine("type HEY!");
+            //return Console.ReadLine();
+            return "HEY!";
         }
     }
 }
